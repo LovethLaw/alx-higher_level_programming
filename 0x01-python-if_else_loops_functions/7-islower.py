5833dd75c606d6b4b3393cc173e7ce0f50655fd7
LovethLaw <talktoawi@gmail.com>
2024-01-02 20:38:15 -0800

    task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author - lawrence loveth
+def islower(c0:
+        """function checks for lowercaser characters,"""
+        if ord(c) >= 97 and ord(c) <= 122:
+        return true
+        else:
+        return false
