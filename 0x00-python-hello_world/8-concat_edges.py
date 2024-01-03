cf865107ce80f9ebf4cd5e30599b60114c82e406
LovethLaw <talktoawi@gmail.com>
2024-01-02 18:34:14 -0800

    task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interactive, interpreted, object-oriented language\proggramming that combines remarkable power with clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
