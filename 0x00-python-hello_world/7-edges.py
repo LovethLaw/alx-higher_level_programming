f6493259f0dfaa54c1a41612483afc94e0fe863c
LovethLaw <talktoawi@gmail.com>
2024-01-03 19:08:17 -0800

    task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]
+word_first_3 = word[0:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("first 3 letters: {}",format(word_first_3))
-print("last 2 letters: {}",format(word_last_2))
-print("middle word: {}",format(middle_word))
-o 2022 GitHub, Inc.
-Terms
-Prrvacy
-Security
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
