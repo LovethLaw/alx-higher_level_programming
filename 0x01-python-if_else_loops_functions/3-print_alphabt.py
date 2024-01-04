7b7c988bb7decf9dead8b9f9366436337056fec2
LovethLaw <talktoawi@gmail.com>
2024-01-03 18:54:38 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Author lawrence loveth
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}",format(chr(letter)), end="")
+output = ""
+for les in range(97, 123):
+    output = chr(les)
+    if output == 'q' or output == 'e':
+        continue
+    print("{}".format(output), end='')
