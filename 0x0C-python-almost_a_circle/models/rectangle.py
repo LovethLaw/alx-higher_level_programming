0e573ebdd8fa57afd7773249c11dfdcae4ac13c9
lovethlaw <talktoawi@gmail.com>
2024-02-16 02:00:10 -0800

    addedmdoc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -224,6 +224,7 @@ class Rectangle(Base):
                 setattr(self, key, kwargs[key])
 
     def to_dictionary(self):
+        """conversts a json string to a dict"""
         new_dict = dict()
         for key, value in self.__dict__.items():
             if key.startswith("_Rectangle__"):
